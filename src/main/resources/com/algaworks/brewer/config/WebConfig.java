29af1f4626ee5c96eac49a2d7aec33ccea25970f
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-04-27 23:08:44 -0300

    Criada classe de configuração web

---
+++ src/main/resources/com/algaworks/brewer/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.algaworks.brewer.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@ComponentScan(basePackageClasses = { CervejasController.class })
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+}
